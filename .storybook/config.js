1856074e302afa5554960a6707b442ea71a53e46
Ned Schwartz <ned@theinterned.net>
2018-11-08 09:54:02 -0500

    configures storybook to colocate stories with implementations

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,13 @@
 import { configure } from '@storybook/react';
 
+const req = require.context(
+  '../src', // path where stories live
+  true, // recursive?
+  /\.story.js$/ // story files match this pattern
+);
+
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(module => req(module));
 }
 
 configure(loadStories, module);
