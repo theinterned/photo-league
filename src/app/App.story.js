5e1496e4a6bf97becf7d725a1ba7b3e30d035fd0
Ned Schwartz <ned@theinterned.net>
2018-11-09 10:40:36 -0500

    removes broken test and story for App

--- src/app/App.story.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import App from './App';
-
-storiesOf('App', module)
-  .add('default', () => (
-    <App />
-  ));
--- src/app/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {mount} from 'enzyme';
-import App from './App';
-
-it('renders without crashing', () => {
-  const app = mount(<App />);
-  expect(app).toMatchSnapshot();
-});
