aca2d98a2bf1af5e8f263582d12f87a474ae7b01
Ned Schwartz <ned@theinterned.net>
2018-11-09 10:56:03 -0500

    adds a link to the full size image

--- src/app/photos/List.js
+++ src/app/photos/List.js
@@ -2,22 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import {Flex, Box} from 'rebass';
+import Link from 'shared/Link';
 
 const Figure = styled('figure')`
   padding: 0;
   margin: 0;
-`;
-
-const Figcaption = styled('figcaption')`
   max-width: 150px;
+  transition: transform .1s ease-in;
+  &:hover {
+    color: blue;
+    text-decoration: underline;
+    transform: scale(1.05);
+  }
 `;
 
-const Picture = ({title, thumbnailUrl}) => <Figure>
-  <img src={thumbnailUrl} alt={`${title}`}/>
-  <Figcaption>{title}</Figcaption>
+const ImgLink = styled('a')`
+  display: block;
+  text-decoration: none;
+  color: inherit;
+`
+
+const Picture = ({title, url, thumbnailUrl}) => <Figure>
+  <ImgLink href={url}><img src={thumbnailUrl} alt={`${title}`}/></ImgLink>
+  <figcaption><ImgLink href={url}>{title}</ImgLink></figcaption>
 </Figure>;
 
 Picture.propTypes = {
+  url: PropTypes.string.isRequired,
   thumbnailUrl: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired
 };
@@ -44,8 +55,8 @@ List.defaultProps = {
 }
 
 
-const PhotoList = ({photos}) => <List>{photos.map(({thumbnailUrl, id, title}) => 
-  <Item key={`photo_${id}`}><Picture title={title} thumbnailUrl={thumbnailUrl} /></Item>
+const PhotoList = ({photos}) => <List>{photos.map(({thumbnailUrl, id, url, title}) => 
+  <Item key={`photo_${id}`}><Picture title={title} url={url} thumbnailUrl={thumbnailUrl} /></Item>
 )}</List>;
 
 List.propTypes = {
