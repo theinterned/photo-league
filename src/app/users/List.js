29bfa1f5ab50c0fb6b4d44d334150e781d13cd61
Ned Schwartz <ned@theinterned.net>
2018-11-09 07:31:06 -0500

    simplifies user List

--- src/app/users/List.js
+++ src/app/users/List.js
@@ -3,17 +3,15 @@ import PropTypes from 'prop-types';
 import {NavList, NavItem} from 'shared/NavList';
 import {makeUserAlbumListUrl} from '../utils/urls';
 
-const Item = ({name, id}) => <NavItem to={makeUserAlbumListUrl(id)}>{name}</NavItem>;
-
-Item.propTypes = {
-  id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired
-};
-
-const List = ({users}) => <NavList>{users.map(({id, name}) => <Item name={name} id={id} key={`user_${id}`}/>)}</NavList>;
+const List = ({users}) => <NavList>{users.map(({id, name}) => 
+  <NavItem to={makeUserAlbumListUrl(id)} key={`user_${id}`}>{name}</NavItem>
+)}</NavList>;
 
 List.propTypes = {
-  users: PropTypes.arrayOf(PropTypes.shape(Item.propTypes))
+  users: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired
+  }))
 };
 
 export default List;
